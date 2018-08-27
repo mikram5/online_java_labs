adbb2d3318b839b86d7e5bc7d2eff305a6b21cec
Michael Ramey <mike.ramey@gmail.com>
2018-08-27 13:43:25 -0500

    code for Ex_01

--- src/datatypes_operators/labs/Exercise_01.java
+++ src/datatypes_operators/labs/Exercise_01.java
@@ -17,6 +17,15 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int count = 5;
+        double d = 67.54;
+        float f = 7;
+        char ch = 'T';
+
+        System.out.println("count: " + count) ;
+        System.out.println("d: " +  d) ;
+        System.out.println("f: " + f) ;
+        System.out.println("ch: " + ch) ;
 
     }
 }
