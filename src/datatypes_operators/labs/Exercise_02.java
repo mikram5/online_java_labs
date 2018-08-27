aaf8c292605a45ce9a4a9d47bdc9b8e77938daeb
Michael Ramey <mike.ramey@gmail.com>
2018-08-27 13:44:14 -0500

    code for Ex_02

--- src/datatypes_operators/labs/Exercise_02.java
+++ src/datatypes_operators/labs/Exercise_02.java
@@ -24,6 +24,16 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // write code here
+        int i = 10;
+        double d = i;
+
+        System.out.println("d: " + d);
+
+        double db = 10.5;
+        int count;
+        count = (int) (db);
+
+        System.out.println("count: " + count);
 
     }
 }
