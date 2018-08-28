23440751e3cd5ce42e00efd5429c46725f2a6fe6
Michael Ramey <mike.ramey@gmail.com>
2018-08-28 16:22:24 -0500

    code to Exercise_04

--- src/conditions_loops/labs/Exercise_04.java
+++ src/conditions_loops/labs/Exercise_04.java
@@ -8,4 +8,13 @@ package conditions_loops.labs;
  */
 
 public class Exercise_04 {
+
+    public static void main(String[] args) {
+        double num;
+
+        for(num = 1; num < 101; num++) {
+            if (num % 2 == 0)
+            System.out.println(num);
+        }
+    }
 }
