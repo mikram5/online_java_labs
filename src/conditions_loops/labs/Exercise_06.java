a865a316005e81ac86091c393189e6002e52b5c9
Michael Ramey <mike.ramey@gmail.com>
2018-08-28 16:23:33 -0500

    code to Exercise_06

--- src/conditions_loops/labs/Exercise_06.java
+++ src/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,15 @@ package conditions_loops.labs;
  */
 
 public class Exercise_06 {
+    public static void main(String args[] ) {
+        int count = 0;
+        int sum = 0;
+
+        while (count <= 100) {
+            sum += count;
+            count ++;
+        }
+
+        System.out.println("The sum of numbers from 1-100 is: " + sum);
+    }
 }
