75d2436d35c7fe664b31c8e3b58c0baf0eff593c
Michael Ramey <mike.ramey@gmail.com>
2018-08-28 16:25:35 -0500

    code to Exercise_10

--- src/conditions_loops/labs/Exercise_10.java
+++ src/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,12 @@ package conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+        int i;
+
+        for(i=0; i<=50; i++) {
+            if(i%2 == 0) continue;
+            System.out.println(i);
+        }
+    }
 }
