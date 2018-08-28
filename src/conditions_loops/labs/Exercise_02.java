dd3fdfcd7a3aca9f5523e4fa96203feba5e4c153
Michael Ramey <mike.ramey@gmail.com>
2018-08-28 16:21:15 -0500

    code to Exercise_02

--- src/conditions_loops/labs/Exercise_02.java
+++ src/conditions_loops/labs/Exercise_02.java
@@ -15,13 +15,19 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // create scanner
-
+        Scanner scanner = new Scanner(System.in);
         // prompt user
-
+        System.out.print("Enter a number: ");
         // assign input to variable as int
-
-
+        int number = scanner.nextInt();
         // write completed code here
-
+        if (number == 1) System.out.println("Monday");
+        else if (number == 2) System.out.println("Tuesday");
+        else if (number == 3) System.out.println("Wednesday");
+        else if (number == 4) System.out.println("Thursday");
+        else if (number == 5) System.out.println("Friday");
+        else if (number == 6) System.out.println("Saturday");
+        else if (number == 7) System.out.println("Sunday");
+        else System.out.println("Other");
     }
 }
