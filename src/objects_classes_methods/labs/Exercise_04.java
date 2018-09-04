0dbdcdb558ca84a182595a82e53f992e5cdd1cb5
Michael Ramey <mike.ramey@gmail.com>
2018-09-04 12:48:46 -0500

    Exercise_04 complete

--- src/objects_classes_methods/labs/Exercise_04.java
+++ src/objects_classes_methods/labs/Exercise_04.java
@@ -7,4 +7,27 @@ package objects_classes_methods.labs;
  *
  */
 
+public class Exercise_04 {
+
+        int a = 35;
+        int b = 12;
+        int c = 7;
+
+        int addition (int a, int b) {
+            return a + b;
+        }
+
+        int addition (int a, int b, int c){
+            return a + b + c;
+        }
+
+        int addition (double a, double b, double c){
+            return ((int) (a + b + c));
+        }
+
+    }
+
+
+
+
 
