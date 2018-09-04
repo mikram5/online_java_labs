1c2d971aded15f2fda0286a9c43686fd45e11dd0
Michael Ramey <mike.ramey@gmail.com>
2018-09-04 12:51:42 -0500

    Exercise_03 class3 complete

---
+++ src/objects_classes_methods/labs/Vegetable.java
@@ -0,0 +1,17 @@
+package objects_classes_methods.labs;
+
+class Vegetable {
+    private int seedCount;
+
+    public Vegetable(int seedCount) {
+        this.seedCount = seedCount;
+    }
+
+    public int getSeedCount() {
+        return seedCount;
+    }
+
+    public void setSeedCount(int seedCount) {
+        this.seedCount = seedCount;
+    }
+}
