483b427c4f7057c836b4176461e28a52c22b65dc
Michael Ramey <mike.ramey@gmail.com>
2018-09-04 12:50:08 -0500

    Exercise_03 class2 complete

---
+++ src/objects_classes_methods/labs/Fruit1.java
@@ -0,0 +1,20 @@
+package objects_classes_methods.labs;
+
+class Fruit1 {
+    String color;
+    int weight;
+
+    //Fruit1 constructor
+    Fruit1(String c, int w) {
+        color = c;
+        weight = w;
+    }
+
+    public String colorMeth(String s) {
+        return s;
+    }
+
+    public int weightMeth(int w) {
+        return w;
+    }
+}
