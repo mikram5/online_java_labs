226b3c4d42d0b125b1885edb5d7ab7bc89d0d1be
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:09:55 -0500

    Exercise_04 complete

--- src/exception_handling/labs/Exercise_04.java
+++ src/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,25 @@ package exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+class NestExample {
+    public static void main(String[] args) {
+        int numer [] = { 4, 8, 16, 32, 64, 128, 256, 512 };
+        int denom [] = { 2, 0, 4, 3, 0, 8 };
+
+        try {
+            for(int i=0; i < numer.length; i++) {
+                try {
+                    System.out.println(numer[i] + " / " +
+                            denom[i] + " is " +
+                            numer[i]/denom[i]);
+                }
+                catch (ArithmeticException exc) {
+                    System.out.println("Can't divide by 0.");
+                }
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("No matching element found.");
+        }
+    }
+}
