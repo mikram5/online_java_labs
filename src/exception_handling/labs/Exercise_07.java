021c0117004d72d3e5b96658fca434d32f5aef88
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:11:18 -0500

    Exercise_07 complete

--- src/exception_handling/labs/Exercise_07.java
+++ src/exception_handling/labs/Exercise_07.java
@@ -6,3 +6,38 @@ package exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+
+class CustomException extends Exception {
+    int n;
+    int d;
+
+    CustomException(int i, int b) {
+        n = i;
+        d = b;
+    }
+
+    public String toString() {
+        return n + " / " + d + " is not an integer.";
+    }
+}
+
+class CustExceptPractice {
+    public static void main(String[] args) {
+
+        int numer[] = { 2, 8, 11 };
+        int denom[] = { 2, 4, 3};
+
+        for(int i=0; i < numer.length; i++) {
+            try {
+                if(numer[i]%2 != 0)
+                    throw new
+                            CustomException(numer[i], denom[i]);
+            }
+            catch (CustomException exc) {
+                System.out.println(exc);
+            }
+        }
+
+    }
+
+}
