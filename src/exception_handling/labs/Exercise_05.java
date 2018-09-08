3e6bdf81efd91a7ede460f00d5c955f33be2b243
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:10:19 -0500

    Exercise_05 complete

--- src/exception_handling/labs/Exercise_05.java
+++ src/exception_handling/labs/Exercise_05.java
@@ -6,3 +6,15 @@ package exception_handling.labs;
  *      Demonstrate how to throw an exception.
  *
  */
+class ThrowPractice {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Before throw.");
+            throw new ArithmeticException();
+        }
+        catch (ArithmeticException exc) {
+            System.out.println("Catching exception");
+        }
+        System.out.println("The exc has been thrown and caught.");
+    }
+}
