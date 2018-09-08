9abbdaee0678113e21802f0c7cb64330e58fdeb7
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:05:03 -0500

    one package and class for Ex_01

---
+++ src/packages_interfaces/labs/packages_demo1/Tree.java
@@ -0,0 +1,21 @@
+package packages_interfaces.labs.packages_demo1;
+
+import packages_interfaces.labs.packages_demo2.Flower;
+
+public class Tree {
+    public int age;
+    protected String treeType;
+
+    public int getAge() {
+        return age;
+    }
+
+    protected String getTreeType() {
+        return treeType;
+    }
+
+    Flower daffodil = new Flower();
+    ;
+}
+
+
