7eb95b6119c226ddba1785937a60cd201670b637
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:10:47 -0500

    Exercise_06 complete

--- src/exception_handling/labs/Exercise_06.java
+++ src/exception_handling/labs/Exercise_06.java
@@ -1,5 +1,7 @@
 package exception_handling.labs;
 
+import java.sql.SQLOutput;
+
 /**
  * Exception Handling Exercise 6:
  *
@@ -7,3 +9,25 @@ package exception_handling.labs;
  *
  */
 
+class ThrowCatch {
+    public static char prompt (String str)
+        throws java.io.IOException {
+
+        System.out.print(str + ": ");
+        return (char) System.in.read();
+    }
+
+    public static void main(String[] args) {
+        char ch;
+
+        try {
+            ch = prompt("Enter a letter");
+        }
+        catch (java.io.IOException exc) {
+            System.out.println("I/O exception occurred.");
+            ch = 'X';
+        }
+
+        System.out.println("You pressed " + ch);
+    }
+}
