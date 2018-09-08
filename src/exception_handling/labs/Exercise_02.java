60f354efba704d5319913808b4f8d9d8a53762a4
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:09:07 -0500

    Exercise_02 complete

--- src/exception_handling/labs/Exercise_02.java
+++ src/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,32 @@ package exception_handling.labs;
  *
  */
 
+class TryCatch2 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 0;
+
+        TryCatch2 val = new TryCatch2();
+        val.divide(8, 0);
+
+    }
+
+    public int divide(int a, int b) {
+
+        try {
+            return a / b;
+        } catch (ArithmeticException exc) {
+            System.out.println("Division by zero not allowed");
+            return 0;
+        } catch (ArrayIndexOutOfBoundsException exc2) {
+            System.out.println("Index is out of bounds");
+            return 0;
+        } finally {
+            System.out.println("You're done!");
+        }
+    }
+}
+
+
+
 
