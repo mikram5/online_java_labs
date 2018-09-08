02f22545b6da50c0a5e868b96d2cec91b4c02448
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:08:39 -0500

    Exercise_01 complete

--- src/exception_handling/labs/Exercise_01.java
+++ src/exception_handling/labs/Exercise_01.java
@@ -7,3 +7,24 @@ package exception_handling.labs;
  *
  */
 
+class Exception1 {
+    static void newException() {
+        int[] someArray = new int[6];
+
+        System.out.println("This works.");
+
+        someArray[7] = 3;
+    }
+}
+
+class TryClassDemo {
+    public static void main(String[] args) {
+
+        try {
+            Exception1.newException();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Index out of bounds.");
+        }
+    }
+}
