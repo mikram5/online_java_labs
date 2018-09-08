0ed55d414686cd706bda8faffda7aef896875397
Michael Ramey <mike.ramey@gmail.com>
2018-09-08 15:05:33 -0500

    second package and class for Ex_01

---
+++ src/packages_interfaces/labs/packages_demo2/Flower.java
@@ -0,0 +1,16 @@
+package packages_interfaces.labs.packages_demo2;
+
+public class Flower {
+    public int petalCount;
+    private String flowerType;
+
+    public int getPetalCount() {
+        return petalCount;
+    }
+
+    public String getFlowerType() {
+        return flowerType;
+    }
+}
+
+
